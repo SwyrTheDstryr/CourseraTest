6df0b77977e71a9e80218165ad0438773c2f458f
SwyrTheDstryr <46672982+SwyrTheDstryr@users.noreply.github.com>
2022-06-14 22:35:09 +0300

    Final Project

---
+++ Coursera.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+one = 1
+two = 2
+print(one+two)
+
+
+# ## Create a line plot
+# This tutorial shows how to create a line plot
+
+# In[3]:
+
+
+a= [1, 2, 3, 4]
+b= [-1, -2, -3, -4]
+
+
+# In[6]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[7]:
+
+
+plt.plot(a,b)
+
+
+# In[ ]:
+
+
+
+
